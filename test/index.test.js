43d40481b5dd899f08187983d948a2740d4b0195
Muhammad Habib Rohman <mhrohman@live.com>
2017-04-21 08:36:41 +0700

    add: test object and array recursively

--- test/index.test.js
+++ test/index.test.js
@@ -21,3 +21,43 @@ test('should return contain undefined from object value', (t) => {
   const actual = nullfined({ a: 1, b: null });
   t.deepEqual(actual, { a: 1, b: undefined });
 });
+
+test('should return object in object recursively', (t) => {
+  const actual = nullfined({
+    a: 1,
+    b: {
+      a: null,
+      b: 444,
+      c: {
+        d: null,
+        e: null,
+        q: true,
+        z: 'somestring',
+        h: {
+          z: null,
+          a: 'yuhu',
+        },
+      } },
+  });
+  t.deepEqual(actual, {
+    a: 1,
+    b: {
+      a: undefined,
+      b: 444,
+      c: {
+        d: undefined,
+        e: undefined,
+        q: true,
+        z: 'somestring',
+        h: {
+          z: undefined,
+          a: 'yuhu',
+        },
+      } },
+  });
+});
+
+test('should resolve null value in array recursively', (t) => {
+  const actual = nullfined([1, 2, null, 4, [null, true, ['somestring', null]]]);
+  t.deepEqual(actual, [1, 2, undefined, 4, [undefined, true, ['somestring', undefined]]]);
+});
