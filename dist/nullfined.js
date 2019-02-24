940d0f11ce6ff7b7c086dc6a56b2ecebc5aa21fe
Rohman Habib M <mhrohman@live.com>
2019-02-24 10:44:55 +0700

    fix(git): untracted files

--- dist/nullfined.js
+++
@@ -1,2 +0,0 @@
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n(require("vaper")):"function"==typeof define&&define.amd?define(["vaper"],n):(e=e||self).nullfined=n(e.vaper)}(this,function(n){"use strict";return n=n&&n.hasOwnProperty("default")?n.default:n,function(e){return n(null,void 0)(e)}});
-//# sourceMappingURL=nullfined.js.map
